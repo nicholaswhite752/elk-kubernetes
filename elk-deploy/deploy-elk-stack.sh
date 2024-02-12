a6b43fc0d5da18f48ad41964e3499c4666d3bf3a
Nick White <nicholas.white752@gmail.com>
2024-02-12 17:41:55 -0500

    updated script to set up stack

--- elk-deploy/deploy-elk-stack.sh
+++ elk-deploy/deploy-elk-stack.sh
@@ -2,6 +2,9 @@
 
 echo "Starting deployment"
 
+## Build image for log producing app
+docker build -t log-producing-app-go:latest -f ../src/Dockerfile ../src
+
 ## Deploys volumes for logs
 helm upgrade -i  logs-volumes logs-volumes/ --values logs-volumes/values.yaml
 
@@ -16,3 +19,13 @@ helm upgrade -i  filebeat-dev filebeat/ --values filebeat/values.yaml
 
 ## Deploys Kibana
 helm upgrade -i  kibana-dev kibana/ --values kibana/values.yaml
+
+## Deploy app
+helm upgrade -i log-producing-app log-producing-app/ --values log-producing-app/values.yaml
+
+echo ""
+
+# Gets Localhost port for kibana instance
+KIBANA_PORT=`kubectl get svc --all-namespaces -o go-template='{{range .items}}{{range.spec.ports}}{{if .nodePort}}{{.nodePort}}{{"\n"}}{{end}}{{end}}{{end}}'`
+
+echo "Kibana app is running on localhost:$KIBANA_PORT"
